06612c3eaa25faf2ab1d55fedaf3162515f53c3b
v4char <v4char@gmail.com>
2017-10-04 11:41:56 +0200

    semi clean data \n

--- recv.c
+++ recv.c
@@ -5,9 +5,24 @@
 
 FILE *file;
 
+char *clear_data(char *msg)
+{
+    char *pos = strchr(msg, '\n');
+    while (pos)
+    {
+        *pos = ' ';
+        pos = strchr(pos, '\n');
+    }
+
+    strcat(msg, "\n");
+
+    return msg;
+}
+
 void log_data(char *client_addr, int client_port, char *msg)
 {
     char data_to_write[1024];
+    msg = clear_data(msg);
     sprintf(data_to_write, "[%s:%d] ", client_addr, client_port);
     strncat(data_to_write, msg, 512);
     file = fopen(FILE_NAME, "a+");
